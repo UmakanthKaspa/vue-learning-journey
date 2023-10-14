e2ed27a9cdecbce37bc99890b747110ce809c713
umakanth1999 <kaspaumakanth1999@gmail.com>
2023-10-14 18:47:13 +0530

    Add Vue.js 3 Reaction Timer Game code

---
+++ reaction-timer/src/components/Result.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+      <p class="reaction-time">Reaction Time: {{ score }} ms</p>
+      <p :class="rankClass">{{ rank }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ["score"],
+    computed: {
+      rank() {
+        if (this.score < 250) {
+          return "Ninja Fingers";
+        } else if (this.score < 400) {
+          return "Rapid Reflexes";
+        } else {
+          return "Snail Pace...";
+        }
+      },
+      rankClass() {
+        if (this.score < 250) {
+          return "green-text";
+        } else if (this.score < 400) {
+          return "orange-text";
+        } else {
+          return "red-text";
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .reaction-time {
+    font-size: 1.2em;
+    color: #333; 
+    font-weight: bold;
+  }
+  
+  .green-text {
+    font-size: 1.4em;
+    color: green;
+    font-weight: bold;
+  }
+  
+  .orange-text {
+    font-size: 1.4em;
+    color: orange;
+    font-weight: bold;
+  }
+  
+  .red-text {
+    font-size: 1.4em;
+    color: red;
+    font-weight: bold;
+  }
+  </style>
+  

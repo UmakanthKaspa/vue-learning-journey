49f85e6aa3f747d52c2f793e3f7b540c176142fb
umakanth1999 <kaspaumakanth1999@gmail.com>
2023-10-16 11:54:28 +0530

    Added Vue Router setup and implemented basic routing, route parameters, dynamic links, redirects, and programmatic navigation

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error totam unde eos beatae tempore quae dignissimos aut dicta asperiores quibusdam ex labore illo deserunt deleniti nemo, reiciendis temporibus iure exercitationem sapiente. Corrupti voluptatibus porro maiores vitae fugiat? Adipisci dolorem ab earum aliquid molestias consectetur minima quidem dolores corrupti, molestiae, culpa, modi omnis sequi magnam! Inventore, officiis. Nulla consequuntur ex dolore minima doloribus aut voluptate aliquid, vitae molestiae expedita quia blanditiis saepe repellendus error ullam vel sunt fugiat quae ab eius perspiciatis asperiores alias ratione ipsum! Unde quaerat, eos molestias fuga sunt delectus, doloremque omnis, placeat nesciunt voluptatibus similique nihil doloribus.</p>
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
---
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Job details</h1>
+  <p>{{id}}</p>
+  <!-- <p>{{$route.params.id}}</p> -->
+</template>
+
+<script>
+export default {
+    props: ['id'],
+// data(){
+//     return{
+//         id:this.$route.params.id
+//     }
+// }
+ }
+</script>
+
+<style>
+
+</style>
